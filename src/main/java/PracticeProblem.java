6ff423e95146ffd64b9c25cd78974a5dc2776893
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-20 22:06:50 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,59 @@
 public class PracticeProblem {
-
+	//test
 	public static void main(String args[]) {
-
+		System.out.println(validateAge(25));
+		System.out.println(withdraw(50, 200));
+		int[] arr = {10, 20, 30};
+		System.out.println(getElement(arr, 1));
+		System.out.println(validateEmail("kafka@gmail.com"));
 	}
 
-	
+		public static boolean validateAge(int age) {
+			if (age < 0) {
+				throw new IllegalArgumentException("Age cannot be negative.");
+			}
+			If (age > 150) { //oldest person was ~122 yrs old
+				throw new IllegalArgumentException("Age must be realistic");
+			}
+			return true;
+
+		}
+
+		public static double withdraw(double amount, double balance) {
+			if (amount <= 0) {
+				throw new IllegalArgumentException("Amount must be greater than zero");
+			}
+			if (balance - amount < 0) {
+				throw new IllegalStateException("Insufficient funds");
+			}
+			return balance - amount;
+		}	
+
+		public static int getElement(int[] array, int index) {
+			if (array == null) {
+				throw new NullPointerException("Array cannot be null");
+			}
+			if(index < 0 || index >= array.length) {
+				throw new IndexOutOfBoundsException("Index is out of bounds");
+			}
+			return array[index];
+		}
+
+		public static String validateEmail(String email) {
+			if (email == null || email.trim().equals("")) {
+				throw new IllegalArgumentException("Email cannot be empty");
+			}
+			String trimmed = email.trim();
+			if(!trimmed.contains("@")) {
+				IllegalArgumentException("Invalid email format");
+			}
+			int atIndex = trimmed.indexOf("@");
+			int dotIndex = trimmed.indexOf(".", atIndex);
+
+			if (dotIndex == -1) {
+				throw new IllegalArgumentException("Invalid email format")
+			}
+			return trimmed;
+		}
 
 }
